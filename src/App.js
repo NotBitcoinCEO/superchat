f376d8e12589f13c63b2d605087e226ad8fe4ae3
seanryanbyu <seanryan@byui.edu>
2022-10-14 20:38:54 -0700

    change

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
   );
 }
 
-function SignIn() {}
+function SignIn() {
   const signInWithGoogle = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
   } auth.signInWithPopup(provider);
@@ -46,6 +46,7 @@ function SignIn() {}
   return (
     <button onClick={signInWithGoogle}>Sign in with Google</button>
   )
+}
 
 function SignOut() {
   return auth.currentUser && (
@@ -54,7 +55,73 @@ function SignOut() {
 }
 
 
-function ChatRoom() {}
+function ChatRoom() {
+
+  const dummy = useRef()
+
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('');
+
+  const sendMessage = async(e) => {
+
+    else.preventDefault();
+
+    else.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    });
+
+    setFormValue('');
+
+    dummy.current.scrollIntoView({ behavior: 'smooth'});
+
+  }
+
+
+  return (
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+        <div> ref={dummy}</div>
+      </main>
+
+
+      <form onSubmit={sendMessage}>
+
+        <input value={formValue} onChange={} />
+
+        <button type='submit'>O</button>
+
+      </form>
+    
+    </>
+  )
+}
+function ChatMessage(Props) {
+  const { text, uid, photoURL } = props.message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return (
+    <div className={'message ${messageClass}'}>
+
+      <p>{text}</p>
+    </div>
+  )
+
+
+}
 
 export default App;
 
